a41eafde5796ffb408259f822a4034f54b133bac
Daksh Mohan <dakshmohan11@gmail.com>
2025-01-21 21:51:47 +0530

    Created basic layout of home page

--- frontend/components/ui/home.js
+++ frontend/components/ui/home.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-function home() {
+function Home() {
   return (
     <>
-    <div className='w-full h-full bg-gray-200'>
+      <div
+        className="w-screen h-screen bg-cover bg-center relative "
+        style={{
+          backgroundImage: "url('/images/IndiaPic.jpg')",
+        }}
+      >
 
-    </div>
+        <div className="absolute inset-y-24 right-12 h-60 w-72 bg-gradient-to-b from-blue-500/70 via-blue-700/50 to-blue-900/30 text-white p-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui et aliquam omnis doloremque blanditiis minima magnam accusamus placeat, optio ab dolorum amet quam ex numquam error consequatur mollitia architecto obcaecati.
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default home
+export default Home;
