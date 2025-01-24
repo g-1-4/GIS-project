9253f6b6c87f46249ed70c91f83d4c732bff6076
Gowtham Sriram <garepall@gitam.in>
2025-01-24 09:38:33 +0530

    changed title of page

--- frontend/components/ui/navbar.js
+++ frontend/components/ui/navbar.js
@@ -6,7 +6,7 @@ function navbar() {
         <>
             <div className="w-full h-16 bg-gradient-to-b from-black to to-black/10   flex flex-row justify-between items-center">
                 <div className=" flex items-center justify-center w-72 h-16 text-white">(Logo)</div>
-                <div className=" flex items-center justify-center  w-72 h-16 font-bold text-4xl text-white ">GIS Land Parcel</div>
+                <div className=" flex items-center justify-center  w-90 h-16 font-bold text-4xl text-white ">Land Parcel Management</div>
                 <div className=" flex flex-row items-center justify-evenly  w-72 h-16 pr-6">
                     <Link href="/">
                         <div className=" w-20 flex items-center justify-center font-bold text-white hover:text-green-500 ">Home</div>
