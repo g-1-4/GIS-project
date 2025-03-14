5b86e979246aad0f58c4b7c480e75aea3a8e2078
abroxus <garepall@gitam.in>
2025-03-14 15:00:08 +0530

    Remove unused components and authentication context to streamline the application

--- gisapp/src/App.js
+++ gisapp/src/App.js
@@ -1,28 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import MapComponent from "./pages/Mapcomponent";  // ✅ Updated import
-import ProtectedRoute from "./pages/ProtectedRoute";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
+import React from 'react';
+import MapComponent from './Components/Mapcomponent'; 
+
+
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <div style={{ height: "100vh" }}>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/map" element={<ProtectedRoute><MapComponent /></ProtectedRoute>} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-    </AuthProvider>
+    <div style={{ height: '100vh' }}>
+      <MapComponent />
+    </div>
   );
 }
 
